47f3c0ddad92e430cca3629cbeef54a94e25727c
Denis Mignanelli <denistschurtschenthaler@gmail.com>
2020-02-04 20:50:26 +0100

    files

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName('discord')[0].addEventListener('mouseover', (e) => {
+    document.getElementsByClassName('discord')[0].innerText = 'iGoof#0982'
+})
+document.getElementsByClassName('discord')[0].addEventListener('mouseleave', (e) => {
+    document.getElementsByClassName('discord')[0].innerText = 'Discord'
+})
+
+document.getElementsByClassName('email')[0].addEventListener('mouseover', (e) => {
+    document.getElementsByClassName('email')[0].innerText = 'denistschurtschenthaler@gmail.com'
+})
+document.getElementsByClassName('email')[0].addEventListener('mouseleave', (e) => {
+    document.getElementsByClassName('email')[0].innerText = 'E-Mail'
+})
